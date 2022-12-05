db45088ab0bcac783cb1d01a0efd13fd6a028c4a
Deri <deri@Deris-MacBook-Air.local>
2022-12-05 20:30:06 +0700

    importing semantic ui video 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Container, Header, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Halo</h1>
-    </div>
+    <Container>
+      <Header as='h1'>Budget</Header>
+      <Statistic size='small'>
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,500.53</Statistic.Value>
+      </Statistic>
+    </Container>
   );
 }
 
